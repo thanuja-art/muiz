14feb14fa1838cafe353dcb433501ea394c69975
thanuja-art <139566869+thanuja-art@users.noreply.github.com>
2023-07-14 19:58:46 +0530

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onSound(DetectedSound.Loud, function () {
+	
+})
+let muiz: music.Playable = null
+music.play(muiz, music.PlaybackMode.UntilDone)
+basic.forever(function () {
+    basic.showString("Hello!")
+})
